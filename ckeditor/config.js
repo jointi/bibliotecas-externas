d0186a9f98c1d8da30bfdd5a289b52776f22fb8a
Join <join-ti@join-ti.com.br>
2014-12-02 11:27:51 -0200

    Removido abas Link e Avançado, da função Imagem. Alterações na toolbar.

--- ckeditor/config.js
+++ ckeditor/config.js
@@ -3,34 +3,35 @@
  * For licensing, see LICENSE.md or http://ckeditor.com/license
  */
 
-CKEDITOR.editorConfig = function( config ) {
+CKEDITOR.editorConfig = function (config) {
     var link = baseUrl();
 
-    config.filebrowserBrowseUrl = link+'vendor/jointi/bibliotecas-externas/ckeditor/ckfinder/ckfinder.html',
-    config.filebrowserImageBrowseUrl = link+'vendor/jointi/bibliotecas-externas/ckeditor/ckfinder/ckfinder.html?type=Imagens',
-    config.filebrowserFlashBrowseUrl = link+'vendor/jointi/bibliotecas-externas/ckeditor/ckfinder/ckfinder.html?type=Flash',
-    config.filebrowserUploadUrl = link+'vendor/jointi/bibliotecas-externas/ckeditor/ckfinder/core/connector/php/connector.php?command=QuickUpload&type=Arquivos',
-    config.filebrowserImageUploadUrl = link+'vendor/jointi/bibliotecas-externas/ckeditor/ckfinder/core/connector/php/connector.php?command=QuickUpload&type=Imagens',
-    config.filebrowserFlashUploadUrl = link+'vendor/jointi/bibliotecas-externas/ckeditor/ckfinder/core/connector/php/connector.php?command=QuickUpload&type=Flash',
-    config.extraPlugins = 'youtube','justify';
+    config.language = 'pt-BR';
+    config.filebrowserBrowseUrl = link + 'vendor/jointi/bibliotecas-externas/ckeditor/ckfinder/ckfinder.html',
+            config.filebrowserImageBrowseUrl = link + 'vendor/jointi/bibliotecas-externas/ckeditor/ckfinder/ckfinder.html?type=Imagens',
+            config.filebrowserFlashBrowseUrl = link + 'vendor/jointi/bibliotecas-externas/ckeditor/ckfinder/ckfinder.html?type=Flash',
+            config.filebrowserUploadUrl = link + 'vendor/jointi/bibliotecas-externas/ckeditor/ckfinder/core/connector/php/connector.php?command=QuickUpload&type=Arquivos',
+            config.filebrowserImageUploadUrl = link + 'vendor/jointi/bibliotecas-externas/ckeditor/ckfinder/core/connector/php/connector.php?command=QuickUpload&type=Imagens',
+            config.filebrowserFlashUploadUrl = link + 'vendor/jointi/bibliotecas-externas/ckeditor/ckfinder/core/connector/php/connector.php?command=QuickUpload&type=Flash',
+            config.extraPlugins = 'youtube', 'justify';
     config.allowedContent = true;
-    // config.extraPlugins = 'oembed';
+    config.extraPlugins = 'timestamp';
 
-    config.toolbarGroups = [
-        { name: 'clipboard', groups: [ 'clipboard', 'undo' ] },
-        { name: 'editing', groups: [ 'find', 'selection', 'spellchecker' ] },
-        { name: 'links' },
-        { name: 'insert' },
-        { name: 'tools' },
-        { name: 'document', groups: [ 'mode', 'document', 'doctools' ] },
-        { name: 'others' },
+    config.toolbar = [
+        {name: 'document', groups: ['mode', 'document', 'doctools'], items: ['Source']},
+        {name: 'clipboard', groups: ['clipboard', 'undo'], items: ['Cut', 'Copy', 'Paste', 'PasteText', 'PasteFromWord', '-', 'Undo', 'Redo']},
+        {name: 'editing', groups: ['find', 'selection', 'spellchecker'], items: ['Find', 'Replace', '-', 'SelectAll']},
         '/',
-        { name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
-        { name: 'paragraph', groups: [ 'list', 'indent', 'blocks', 'align'] },
-        { name: 'colors' },
-        { name: 'about' }
+        {name: 'basicstyles', groups: ['basicstyles', 'cleanup'], items: ['Bold', 'Italic', 'Underline', 'Strike', 'Subscript', 'Superscript', '-', 'RemoveFormat']},
+        {name: 'paragraph', groups: ['list', 'indent', 'blocks', 'align', 'bidi'], items: ['NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', '-', 'Blockquote', '-', 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock']},
+        {name: 'links', items: ['Link', 'Unlink']},
+        {name: 'insert', items: ['Image', 'Table', 'HorizontalRule', 'SpecialChar', 'Iframe']},
+        '/',
+        {name: 'styles', items: ['Styles', 'Format', 'Font', 'FontSize']},
+        {name: 'colors', items: ['TextColor', 'BGColor']},
+        {name: 'tools', items: ['Maximize', 'ShowBlocks', 'Timestamp']}
     ];
 
     // Use the classes 'AlignLeft', 'AlignCenter', 'AlignRight', 'AlignJustify'
-    config.justifyClasses = [ 'AlignLeft', 'AlignCenter', 'AlignRight', 'AlignJustify' ];
+    config.justifyClasses = ['AlignLeft', 'AlignCenter', 'AlignRight', 'AlignJustify'];
 };
