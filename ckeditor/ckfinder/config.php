a71f4081bd6f13cf86d37a31f15ac2db7a2556d7
Join <join-ti@join-ti.com.br>
2014-12-05 14:59:30 -0200

    Restringido upload de imagens em Links, de acordo com solicitação do IPHAN.

--- ckeditor/ckfinder/config.php
+++ ckeditor/ckfinder/config.php
@@ -261,7 +261,8 @@ $config['ResourceType'][] = Array(
 		'url' => $baseUrl . 'arquivos',
 		'directory' => $baseDir . 'arquivos',
 		'maxSize' => 0,
-		'allowedExtensions' => '7z,aiff,asf,avi,bmp,csv,doc,docx,fla,flv,gif,gz,gzip,jpeg,jpg,mid,mov,mp3,mp4,mpc,mpeg,mpg,ods,odt,pdf,png,ppt,pptx,pxd,qt,ram,rar,rm,rmi,rmvb,rtf,sdc,sitd,swf,sxc,sxw,tar,tgz,tif,tiff,txt,vsd,wav,wma,wmv,xls,xlsx,zip',
+//		'allowedExtensions' => '7z,aiff,asf,avi,bmp,csv,doc,docx,fla,flv,gif,gz,gzip,jpeg,jpg,mid,mov,mp3,mp4,mpc,mpeg,mpg,ods,odt,pdf,png,ppt,pptx,pxd,qt,ram,rar,rm,rmi,rmvb,rtf,sdc,sitd,swf,sxc,sxw,tar,tgz,tif,tiff,txt,vsd,wav,wma,wmv,xls,xlsx,zip',
+                'allowedExtensions' => '7z,aiff,asf,avi,csv,doc,docx,fla,flv,gz,gzip,mid,mov,mp3,mp4,mpc,mpeg,mpg,ods,odt,pdf,ppt,pptx,pxd,qt,ram,rar,rm,rmi,rmvb,rtf,sdc,sitd,swf,sxc,sxw,tar,tgz,txt,vsd,wav,wma,wmv,xls,xlsx,zip',
 		'deniedExtensions' => 'exe,com');
 
 /*
